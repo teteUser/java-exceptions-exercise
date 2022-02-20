d52f7d67a52f1b43a15a25a138b49df0a4b700c4
Tennison Capra <tennison_capra@hotmail.com>
2022-02-20 09:35:42 -0300

    starting exceptions handling exercise from lesson #146

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+        scan.close();
+    }
+
+}
