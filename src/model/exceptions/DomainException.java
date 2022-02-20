fa59ea3d8d96d05cb851d559666ee6de717a1a65
Tennison Capra <tennison_capra@hotmail.com>
2022-02-20 09:38:53 -0300

    exceptions exercise lesson #146

--- src/model/exceptions/DomainException.java
+++ src/model/exceptions/DomainException.java
@@ -2,6 +2,10 @@ package model.exceptions;
 
 public class DomainException extends RuntimeException{
 
+    private static final long serialVersionUID = 1L;
 
+    public DomainException(String msg{
+        super(msg);
+    }
 
 }
